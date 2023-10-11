65e31544aeea2b66c88b533851a15ccb5b97e952
Daniel Zawada <daniel.zawada@usask.ca>
2023-10-11 15:33:59 -0600

    again

--- ci/cibw/cibw_test_script.sh
+++ ci/cibw/cibw_test_script.sh
@@ -4,4 +4,4 @@ PROJECT_DIR="$1"
 
 python -c "import sasktran2 as sk; sk.appconfig.download_standard_databases()"
 
-pytest $PROJECT_DIR/tests
+pytest -W 'once:The distutils package is deprecated:DeprecationWarning' $PROJECT_DIR/tests
