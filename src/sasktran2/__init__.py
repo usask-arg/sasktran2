9b3bb71e64dcddb101244f0f2c67f5e9ff94dcf8
Daniel Zawada <daniel.zawada@usask.ca>
2023-10-12 09:27:24 -0600

    update

--- src/sasktran2/__init__.py
+++ src/sasktran2/__init__.py
@@ -11,7 +11,7 @@ from ._core import AtmosphereStokes_1, AtmosphereStokes_3
 from .atmosphere import Atmosphere
 
 from ._core import OutputIdealStokes_1, OutputIdealStokes_3
-from .output import Output, OutputIdealß
+from .output import Output, OutputIdeal
 
 from ._core import EngineStokes_1, EngineStokes_3
 from .engine import Engine
