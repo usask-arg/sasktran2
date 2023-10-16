62118fc0698c36e273b1a7c2a6a11e9c4890b757
Daniel Zawada <daniel.zawada@usask.ca>
2023-10-16 08:23:14 -0600

    Add linear interpolating matrix function

---
+++ src/sasktran2/util/interpolation.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def linear_interpolating_matrix(
+    from_grid: np.array, to_grid: np.array, out_of_bounds_mode: str
+) -> np.ndarray:
+    """
+    Constructs an interpolating matrix that can be used to interpolate from one grid to another.
+
+    Parameters
+    ----------
+    from_grid : np.array
+        Grid with the original values. Must be sorted in ascending order
+    to_grid : np.array
+        Grid we are going to interpolate to. Must be sorted in ascending order
+    out_of_bounds_mode : str
+        One of ["zero", "extend"], "zero" will set the result to 0 outside the bounds of the original grid,
+        "extend" will extend the lowest and highest values
+
+    Returns
+    -------
+    np.ndarray
+        A matrix such that M @ (values on from_grid) = (values on to_grid)
+    """
+    M = np.zeros((len(to_grid), len(from_grid)))
+
+    for idx, ele in enumerate(to_grid):
+        if ele < from_grid[0] or ele > from_grid[-1]:
+            if out_of_bounds_mode == "zero":
+                continue
+            if out_of_bounds_mode == "extend":
+                if ele < from_grid[0]:
+                    M[idx, 0] = 1
+                else:
+                    M[idx, -1] = 1
+                continue
+            msg = f"Invalid out_of_bounds_mode {out_of_bounds_mode}"
+            raise ValueError(msg)
+
+        # else we are inside the grid
+        idx_above = np.nonzero(from_grid > ele)[0]
+        idx_above = len(from_grid) if len(idx_above) == 0 else idx_above[0]
+
+        interp_ele = ele
+        if idx_above == 0:
+            idx_above = 1
+            interp_ele = from_grid[0]
+
+        if idx_above == len(from_grid):
+            M[idx, idx_above - 1] = 1
+        else:
+            w = (from_grid[idx_above] - interp_ele) / (
+                from_grid[idx_above] - from_grid[idx_above - 1]
+            )
+            M[idx, idx_above] = 1 - w
+            M[idx, idx_above - 1] = w
+
+    return M
---
+++ tests/util/test_interpolation.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sasktran2 as sk
+
+
+def test_linear_interpolating_matrix():
+    """
+    Test that the linear_interpolating_matrix function works as expected by comparing against the
+    numpy interp function
+    """
+    from_grid = np.array([0.0, 1000.0, 3000.0, 5000.0, 10000.0])
+
+    to_grid = np.array([-20.0, 500.0, 3000.0, 4000.0, 8000.0, 12000.0])
+
+    rng = np.random.default_rng()
+    vals = rng.random(len(from_grid))
+
+    M = sk.util.interpolation.linear_interpolating_matrix(from_grid, to_grid, "extend")
+    np.testing.assert_array_almost_equal(
+        M @ vals, np.interp(to_grid, from_grid, vals, left=vals[0], right=vals[-1])
+    )
+
+    M = sk.util.interpolation.linear_interpolating_matrix(from_grid, to_grid, "zero")
+    np.testing.assert_array_almost_equal(
+        M @ vals, np.interp(to_grid, from_grid, vals, left=0, right=0)
+    )
