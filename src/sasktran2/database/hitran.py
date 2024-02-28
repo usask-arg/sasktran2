8c004acca5c6506f10c4e3da0b53b81ad5eaaf61
TaranWarnock <TaranWarnock@users.noreply.github.com>
2024-02-28 09:59:28 -0600

    HAPI Backend (#74)
    Adds the HITRAN API (HAPI) as a backend for HITRAN cross section
    calculation. This requires the `hitran-api` python package which is
    added as an optional dependency. HITRAN line files are downloaded for
    each molecule as needed and saved in a folder called `hitran_lines` in
    the root database folder.
    
    ---------
    
    Co-authored-by: Daniel Zawada <daniel.zawada@usask.ca>

--- src/sasktran2/database/hitran.py
+++ src/sasktran2/database/hitran.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import logging
 from pathlib import Path
 
 import numpy as np
@@ -116,6 +117,86 @@ PRESSURE_GRID = (
 TEMP_GRID = np.arange(190, 311, 10)
 
 
+class HITRANLineDatabase(CachedDatabase):
+    def __init__(
+        self, db_root: Path | None = None, rel_path: Path | None = "hitran_lines"
+    ):
+        CachedDatabase.__init__(self, db_root, rel_path=rel_path)
+
+    def path(self, key: str, **kwargs) -> Path:
+        data_file = self._db_root.joinpath(f"{key}.data")
+        header_file = self._db_root.joinpath(f"{key}.header")
+        if not (data_file.exists() and header_file.exists()):
+            # download lines for this molecule
+            self._download_line_db(key)
+
+    def load_ds(self, key: str, **kwargs) -> xr.Dataset:
+        try:
+            import hapi
+        except ImportError as err:
+            msg = "HITRAN API is required to use HAPI, try pip install hitran-api"
+            raise ImportError(msg) from err
+
+        self.initialize_hapi(key)
+
+        data_vars = {}
+        for param in hapi.PARLIST_ALL:
+            try:
+                param_column = hapi.getColumn(key, param)
+                data_vars[param] = ("line", param_column)
+            except KeyError:  # param does not exist for this molecule
+                continue
+        return xr.Dataset(data_vars=data_vars)
+
+    def clear(self):
+        # deletes all .data and .header files in database folder
+        for header_file in self._db_root.glob("*.header"):
+            header_file.unlink()
+        for data_file in self._db_root.glob("*.data"):
+            data_file.unlink()
+
+    def _download_line_db(self, molecule):
+        try:
+            import hapi
+        except ImportError as err:
+            msg = "HITRAN API is required to use HAPI, try pip install hitran-api"
+            raise ImportError(msg) from err
+
+        self.initialize_hapi()
+
+        # Get global isotopologue ids for this molecule
+        mol_index = hapi.ISO_ID_INDEX["mol_name"]
+        iso_ids = []
+        for id in hapi.ISO_ID:
+            if hapi.ISO_ID[id][mol_index] == molecule:
+                iso_ids.append(id)
+
+        # Download files
+        hapi.fetch_by_ids(
+            molecule, iso_ids, 0.0, 1.0e6, ParameterGroups=["par_line", "sdvoigt", "ht"]
+        )
+
+        return
+
+    def initialize_hapi(self, table_name: str | None = None):
+        """
+        Initialize the HAPI database with one or no tables. This is to reduce the time and memory that hapi.db_begin
+        uses when it is given a folder with many data files.
+        """
+        try:
+            import hapi
+        except ImportError as err:
+            msg = "HITRAN API is required to use HAPI, try pip install hitran-api"
+            raise ImportError(msg) from err
+
+        hapi.VARIABLES["BACKEND_DATABASE_NAME"] = str(self._db_root)
+        hapi.LOCAL_TABLE_CACHE = {}
+        if table_name is not None:
+            hapi.storage2cache(table_name)
+
+        return
+
+
 class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
     def __init__(
         self,
@@ -126,6 +207,7 @@ class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
         reduction_factor: int,
         db_root: Path | None = None,
         backend: str = "sasktran_legacy",
+        profile: str = "voigt",
     ) -> None:
         """
 
@@ -136,7 +218,11 @@ class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
             The root directory to store the database, by default None
         backend : str, optional
             The backend to use, by default "sasktran_legacy" which requires the module `sasktran` to be installed. Currently supported
-            options are ["sasktran_legacy"]. Default is "sasktran_legacy"
+            options are ["sasktran_legacy", "hapi"]. Default is "sasktran_legacy"
+        profile : str, optional
+            The line shape profile to use in the cross section calculation. Currently, only the "hapi" backend supports options other
+            than the default "voigt". Supported options are ["voigt", "sdvoigt", "ht", "priority", "lorentz", "doppler"].
+            Default is "voigt".
         kwargs
             Additional arguments to pass to the particle size distribution, these should match the psize_distribution.args() method
         """
@@ -147,6 +233,14 @@ class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
                     return obj.tolist()
                 return json.JSONEncoder.default(self, obj)
 
+        self._profile = profile.lower()
+        if backend == "sasktran_legacy" and self._profile != "voigt":
+            logging.warning(
+                "%s line profile is not supported by sasktran_legacy backend. Switching to voigt",
+                self._profile,
+            )
+            self._profile = "voigt"
+
         hasher = hashlib.sha1()
         encoded = json.dumps(
             {
@@ -154,6 +248,7 @@ class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
                 "end_wavenumber": end_wavenumber,
                 "wavenumber_resolution": wavenumber_resolution,
                 "reduction_factor": reduction_factor,
+                "profile": self._profile,
             },
             sort_keys=True,
             cls=NumpyEncoder,
@@ -167,9 +262,13 @@ class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
         self._reduction_factor = reduction_factor
         self._molecule = molecule
         self._ltol = 1e-9
+        self._wavenumber_wing = 50
+        self._wavenumber_margin = 10
 
         CachedDatabase.__init__(
-            self, db_root=db_root, rel_path=Path("hitran").joinpath(self._molecule)
+            self,
+            db_root=db_root,
+            rel_path=Path("hitran").joinpath(self._molecule, self._backend),
         )
 
         self._data_file = self._db_root.joinpath(f"{identifier}.nc")
@@ -179,6 +278,8 @@ class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
     def generate(self):
         if self._backend == "sasktran_legacy":
             self._generate_sasktran_legacy()
+        elif self._backend == "hapi":
+            self._generate_hapi()
         else:
             msg = f"Invalid backend {self._backend}"
             raise ValueError(msg)
@@ -229,6 +330,68 @@ class HITRANDatabase(CachedDatabase, OpticalDatabaseGenericAbsorber):
                     .total
                     / 1e4
                 )
+
+        ds = xr.Dataset(
+            {"xs": (["pressure", "temperature", "wavenumber_cminv"], xs)},
+            coords={
+                "pressure": PRESSURE_GRID,
+                "temperature": TEMP_GRID,
+                "wavenumber_cminv": hires_wavenumber_grid,
+            },
+        )
+
+        ds.to_netcdf(self._data_file)
+
+    def _generate_hapi(self):
+        line_db = HITRANLineDatabase()
+        line_db.path(self._molecule)
+
+        try:
+            import hapi
+        except ImportError as err:
+            msg = "HITRAN API is required to use HAPI, try pip install hitran-api"
+            raise ImportError(msg) from err
+
+        abs_cal_fn = {
+            "voigt": hapi.absorptionCoefficient_Voigt,
+            "sdvoigt": hapi.absorptionCoefficient_SDVoigt,
+            "ht": hapi.absorptionCoefficient_HT,
+            "priority": hapi.absorptionCoefficient_Priority,
+            "lorentz": hapi.absorptionCoefficient_Lorentz,
+            "doppler": hapi.absorptionCoefficient_Doppler,
+        }
+        if self._profile not in abs_cal_fn:
+            msg = f"Invalid line profile {self._profile}"
+            raise ValueError(msg)
+
+        line_db.initialize_hapi(self._molecule)
+
+        hapi.select(
+            self._molecule,
+            DestinationTableName="spectral_window",
+            Conditions=(
+                "between",
+                "nu",
+                self._start_wavenumber - self._wavenumber_margin,
+                self._end_wavenumber + self._wavenumber_margin,
+            ),
+        )
+
+        hires_wavenumber_grid = np.arange(
+            self._start_wavenumber, self._end_wavenumber, self._wavenumber_resolution
+        )
+
+        xs = np.zeros((len(PRESSURE_GRID), len(TEMP_GRID), len(hires_wavenumber_grid)))
+
+        for idx, pres in enumerate(PRESSURE_GRID):
+            for idy, temp in enumerate(TEMP_GRID):
+                _, xs_hapi = abs_cal_fn[self._profile](
+                    SourceTables="spectral_window",
+                    Environment={"T": temp, "p": pres / 101325.0},
+                    WavenumberGrid=hires_wavenumber_grid.tolist(),
+                    WavenumberWing=self._wavenumber_wing,
+                )
+                xs[idx, idy] = xs_hapi / 1e4
 
         ds = xr.Dataset(
             {"xs": (["pressure", "temperature", "wavenumber_cminv"], xs)},
