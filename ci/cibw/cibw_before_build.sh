e747df97a50f173f1a7f3e60cb2284a963844870
Daniel Zawada <daniel.zawada@usask.ca>
2024-08-08 11:47:16 -0600

    Let's just try building our open openblas

--- ci/cibw/cibw_before_build.sh
+++ ci/cibw/cibw_before_build.sh
@@ -4,51 +4,26 @@ set -xe
 PROJECT_DIR="${1:-$PWD}"
 
 
-# remove any cruft from a previous run
-rm -rf build
-
-if [[ $(python -c"import sys; print(sys.maxsize)") < $(python -c"import sys; print(2**33)") ]]; then
-    echo "No BLAS used for 32-bit wheels"
-    export INSTALL_OPENBLAS=false
-elif [ -z $INSTALL_OPENBLAS ]; then
-    # the macos_arm64 build might not set this variable
-    export INSTALL_OPENBLAS=true
-fi
-
-# Install Openblas from scipy-openblas64
-if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
-    echo PKG_CONFIG_PATH $PKG_CONFIG_PATH
-    # PKG_CONFIG_PATH=$PROJECT_DIR/.openblas
-    # rm -rf $PKG_CONFIG_PATH
-    # mkdir -p $PKG_CONFIG_PATH
-
-    # Check if the directory does not exist
-    if [ ! -d "$PKG_CONFIG_PATH" ]; then
-    # Create the directory
-        mkdir -p "$PKG_CONFIG_PATH"
-    fi
-
-    python -m pip install scipy-openblas64==0.3.27.44.3
-    python -c "import scipy_openblas64; print(scipy_openblas64.get_pkg_config())" > $PKG_CONFIG_PATH/scipy-openblas.pc
-    # Copy the shared objects to a path under $PKG_CONFIG_PATH, the build
-    # will point $LD_LIBRARY_PATH there and then auditwheel/delocate-wheel will
-    # pull these into the wheel. Use python to avoid windows/posix problems
-    python <<EOF
-import os, scipy_openblas64, shutil
-srcdir = os.path.join(os.path.dirname(scipy_openblas64.__file__), "lib")
-shutil.copytree(srcdir, os.path.join("$PKG_CONFIG_PATH", "lib"))
-srcdir = os.path.join(os.path.dirname(scipy_openblas64.__file__), ".dylibs")
-if os.path.exists(srcdir):  # macosx delocate
-    shutil.copytree(srcdir, os.path.join("$PKG_CONFIG_PATH", ".dylibs"))
-EOF
-    # pkg-config scipy-openblas --print-provides
-fi
 if [[ $RUNNER_OS == "Windows" ]]; then
     # delvewheel is the equivalent of delocate/auditwheel for windows.
     python -m pip install delvewheel wheel
 
     # Need pkg-config to use scipy openblas
     choco install pkgconfiglite
+fi
+
+
+if [[ $RUNNER_OS == "Linux" ]]; then
+    # Install same version of gfortran as the openblas-libs builds
+    if [[ $PLATFORM == "macosx-arm64" ]]; then
+        PLAT="arm64"
+    fi
+    source $PROJECT_DIR/ci/cibw/gfortran_utils.sh
+    install_gfortran
+    pip install "delocate==0.10.4"
+
+    cd ~ && wget -nv https://github.com/xianyi/OpenBLAS/releases/download/v0.3.18/OpenBLAS-0.3.18.tar.gz && tar xf OpenBLAS-0.3.18.tar.gz
+    mkdir ~/OpenBLAS-0.3.18/build && cd ~/OpenBLAS-0.3.18/build && cmake ../ -DBUILD_SHARED_LIBS:bool=OFF -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true -DLAPACKE:BOOL=on && cmake --build . && cmake --install .
 fi
 
 if [[ $RUNNER_OS == "macOS" ]]; then
@@ -59,4 +34,7 @@ if [[ $RUNNER_OS == "macOS" ]]; then
     source $PROJECT_DIR/ci/cibw/gfortran_utils.sh
     install_gfortran
     pip install "delocate==0.10.4"
+
+    cd ~ && wget -nv https://github.com/xianyi/OpenBLAS/releases/download/v0.3.18/OpenBLAS-0.3.18.tar.gz && tar xf OpenBLAS-0.3.18.tar.gz
+    mkdir ~/OpenBLAS-0.3.18/build && cd ~/OpenBLAS-0.3.18/build && cmake ../ -DBUILD_SHARED_LIBS:bool=OFF -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=true -DLAPACKE:BOOL=on && cmake --build . && cmake --install .
 fi
