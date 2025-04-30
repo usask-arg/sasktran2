17cacf919d770f07fb4bc41762a7fdd54bbc82de
Daniel Zawada <daniel.zawada@usask.ca>
2025-04-30 14:38:52 -0600

    bump

--- rust/sasktran2-sys/build.rs
+++ rust/sasktran2-sys/build.rs
@@ -41,5 +41,6 @@ fn main() {
         .expect("Couldn't write bindings!");
 
     // Also link C++ stdlib if needed
+    println!("cargo:rustc-link-lib=stdc++");
     // println!("cargo:rustc-link-lib=dylib=stdc++"); // Or libc++ on macOS
 }
