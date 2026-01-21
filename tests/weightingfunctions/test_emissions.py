fix backprop for thermal

tests/weightingfunctions/test_emissions.py
@@ -189,6 +189,131 @@ def _ground_test_scenarios():
     return scen
 
 
+def _ground_test_scenarios_backprop():
+    config = sk.Config()
+    config.emission_source = sk.EmissionSource.Standard
+    config.single_scatter_source = sk.SingleScatterSource.NoSource
+    config.do_backprop = True
+
+    altitude_grid = np.arange(0, 65001, 5000.0)
+
+    geometry = sk.Geometry1D(
+        0.6,
+        0,
+        6327000,
+        altitude_grid,
+        sk.InterpolationMethod.LinearInterpolation,
+        sk.GeometryType.Spherical,
+    )
+
+    viewing_geo = sk.ViewingGeometry()
+
+    viewing_geo.add_ray(sk.GroundViewingSolar(0.6, 0, 0.6, 200000))
+
+    wavelengths = np.arange(7370, 7380, 0.01)
+    hitran_db = sk.optical.database.OpticalDatabaseGenericAbsorber(
+        sk.database.StandardDatabase().path(
+            "hitran/CH4/sasktran2/60fc6c547a1b2e1181f3296dead288d84fa7c178.nc"
+        )
+    )
+
+    hitran_db = sk.database.HITRANDatabase(
+        molecule="CH4",
+        start_wavenumber=1355,
+        end_wavenumber=1357,
+        wavenumber_resolution=0.01,
+        reduction_factor=1,
+        backend="sasktran2",
+        profile="voigt",
+    )
+
+    atmosphere = sk.Atmosphere(geometry, config, wavelengths_nm=wavelengths)
+
+    sk.climatology.us76.add_us76_standard_atmosphere(atmosphere)
+    atmosphere["ch4"] = sk.climatology.mipas.constituent("CH4", hitran_db)
+    atmosphere["emission"] = sk.constituent.ThermalEmission()
+    atmosphere["surface_emission"] = sk.constituent.SurfaceThermalEmission(300, 0.9)
+
+    sk.climatology.us76.add_us76_standard_atmosphere(atmosphere)
+    atmosphere["solar_irradiance"] = sk.constituent.SolarIrradiance()
+
+    atmosphere["rayleigh"] = sk.constituent.Rayleigh()
+
+    scen = []
+
+    scen.append(
+        {
+            "config": config,
+            "geometry": geometry,
+            "viewing_geo": viewing_geo,
+            "atmosphere": atmosphere,
+        }
+    )
+
+    config = sk.Config()
+    config.emission_source = sk.EmissionSource.DiscreteOrdinates
+    config.single_scatter_source = sk.SingleScatterSource.DiscreteOrdinates
+    config.multiple_scatter_source = sk.MultipleScatterSource.DiscreteOrdinates
+    config.num_streams = 4
+    config.num_forced_azimuth = 1
+    config.do_backprop = True
+
+    altitude_grid = np.arange(0, 65001, 5000.0)
+
+    geometry = sk.Geometry1D(
+        0.6,
+        0,
+        6327000,
+        altitude_grid,
+        sk.InterpolationMethod.LinearInterpolation,
+        sk.GeometryType.PlaneParallel,
+    )
+
+    viewing_geo = sk.ViewingGeometry()
+
+    viewing_geo.add_ray(sk.GroundViewingSolar(0.6, 0, 0.6, 200000))
+
+    wavelengths = np.arange(7370, 7380, 0.01)
+    hitran_db = sk.optical.database.OpticalDatabaseGenericAbsorber(
+        sk.database.StandardDatabase().path(
+            "hitran/CH4/sasktran2/60fc6c547a1b2e1181f3296dead288d84fa7c178.nc"
+        )
+    )
+
+    hitran_db = sk.database.HITRANDatabase(
+        molecule="CH4",
+        start_wavenumber=1355,
+        end_wavenumber=1357,
+        wavenumber_resolution=0.01,
+        reduction_factor=1,
+        backend="sasktran2",
+        profile="voigt",
+    )
+
+    atmosphere = sk.Atmosphere(geometry, config, wavelengths_nm=wavelengths)
+
+    sk.climatology.us76.add_us76_standard_atmosphere(atmosphere)
+    atmosphere["ch4"] = sk.climatology.mipas.constituent("CH4", hitran_db)
+    atmosphere["emission"] = sk.constituent.ThermalEmission()
+    atmosphere["surface_emission"] = sk.constituent.SurfaceThermalEmission(300, 0.9)
+
+    sk.climatology.us76.add_us76_standard_atmosphere(atmosphere)
+
+    atmosphere["rayleigh"] = sk.constituent.Rayleigh()
+    atmosphere["solar_irradiance"] = sk.constituent.SolarIrradiance()
+
+    scen.append(
+        {
+            "config": config,
+            "geometry": geometry,
+            "viewing_geo": viewing_geo,
+            "atmosphere": atmosphere,
+        }
+    )
+
+    return scen
+
+
 def test_wf_extinction_ssa_with_emission():
     """
     Checks that the WFs are correct for a VMR constituent When emissions are present
@@ -218,7 +343,9 @@ def test_wf_temperature_with_emission():
     Checks that the WFs are correct for a VMR constituent When emissions are present
     """
 
-    scens = _test_scenarios() + _ground_test_scenarios()
+    scens = (
+        _test_scenarios() + _ground_test_scenarios() + _ground_test_scenarios_backprop()
+    )
 
     for scen in scens:
         atmosphere = scen["atmosphere"]