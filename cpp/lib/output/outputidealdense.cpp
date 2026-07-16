593eeb4ca8eb38a787927a61ba379a831ca99d00
Daniel Zawada <daniel.zawada@usask.ca>
2026-07-16 13:48:03 -0600

    Fix batched output Eigen mappings

--- cpp/lib/output/outputidealdense.cpp
+++ cpp/lib/output/outputidealdense.cpp
@@ -64,14 +64,27 @@ namespace sasktran2 {
 
         const int stride = NSTOKES * this->m_nlos;
         const int first = stride * block.start + NSTOKES * losidx;
-        const auto output_indices = [&](int stokes) {
-            return Eigen::seqN(first + stokes, block.count, stride);
+        const Eigen::InnerStride<Eigen::Dynamic> value_stride(stride);
+        using StridedValueMap = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned,
+                                           Eigen::InnerStride<Eigen::Dynamic>>;
+        const auto output_value = [&](int stokes) {
+            return StridedValueMap(m_radiance.value.data() + first + stokes,
+                                   block.count, value_stride);
+        };
+        using StridedDerivativeMap =
+            Eigen::Map<Eigen::MatrixXd, Eigen::Unaligned,
+                       Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>;
+        const auto output_derivative = [&](int stokes) {
+            const Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>
+                derivative_stride(m_radiance.deriv.outerStride(), stride);
+            return StridedDerivativeMap(
+                m_radiance.deriv.data() + first + stokes, block.count,
+                this->m_nderiv, derivative_stride);
         };
 
-        m_radiance.value(output_indices(0)) =
-            radiance.value.row(0).head(block.count).transpose();
+        output_value(0) = radiance.value.row(0).head(block.count).transpose();
         if (this->m_nderiv > 0) {
-            m_radiance.deriv(output_indices(0), Eigen::placeholders::all) =
+            output_derivative(0) =
                 radiance.derivative_stokes(0, this->m_nderiv, 0, block.count)
                     .transpose();
         }
@@ -79,11 +92,11 @@ namespace sasktran2 {
         if constexpr (NSTOKES >= 3) {
             const double stokes_c = this->m_stokes_C[losidx];
             const double stokes_s = this->m_stokes_S[losidx];
-            m_radiance.value(output_indices(1)) =
+            output_value(1) =
                 (stokes_c * radiance.value.row(1).head(block.count) -
                  stokes_s * radiance.value.row(2).head(block.count))
                     .transpose();
-            m_radiance.value(output_indices(2)) =
+            output_value(2) =
                 (stokes_s * radiance.value.row(1).head(block.count) +
                  stokes_c * radiance.value.row(2).head(block.count))
                     .transpose();
@@ -92,18 +105,18 @@ namespace sasktran2 {
                                                           block.count);
                 const auto u = radiance.derivative_stokes(0, this->m_nderiv, 2,
                                                           block.count);
-                m_radiance.deriv(output_indices(1), Eigen::placeholders::all) =
+                output_derivative(1) =
                     (stokes_c * q - stokes_s * u).transpose();
-                m_radiance.deriv(output_indices(2), Eigen::placeholders::all) =
+                output_derivative(2) =
                     (stokes_s * q + stokes_c * u).transpose();
             }
         }
 
         if constexpr (NSTOKES == 4) {
-            m_radiance.value(output_indices(3)) =
+            output_value(3) =
                 radiance.value.row(3).head(block.count).transpose();
             if (this->m_nderiv > 0) {
-                m_radiance.deriv(output_indices(3), Eigen::placeholders::all) =
+                output_derivative(3) =
                     radiance
                         .derivative_stokes(0, this->m_nderiv, 3, block.count)
                         .transpose();
